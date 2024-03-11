f0979d933fdb8c206a3bad74307e07dec0e7c757
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2024-03-11 07:20:40 +0300

    feat(dark-light mode) : add functionnality for a toggle theme

--- src/sections/navigation/nav-item.tsx
+++ src/sections/navigation/nav-item.tsx
@@ -5,7 +5,8 @@ type NavItemProps = {
 };
 
 export const NavItem = (props: NavItemProps) => {
+ 
   const { label, isActive = false } = props;
 
-  return <button className={`${isActive ? "text-yellow" : "text-white"}`}>{label}</button>;
+  return <button className={`${isActive ? "text-yellow" : "text-blue-dark dark:text-white"}`}>{label}</button>;
 };
